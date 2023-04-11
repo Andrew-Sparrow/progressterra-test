44e440daa08a2cc14d7819109e43ceb9fdb25393
Andrew-Sparrow <8911102@gmail.com>
2023-04-11 13:10:10 +0500

    adjusts paddings in bonus block

--- src/components/bonus/bonus.jsx
+++ src/components/bonus/bonus.jsx
@@ -3,13 +3,24 @@ import { HandySvg } from 'handy-svg';
 import "../../styles/variables.scss";
 import './bonus.scss';
 import chevronRightSVG from '../../icons/chevron_right.svg';
+import fireSVG from '../../icons/fire.svg';
 
 
 const IconChevronRight = () => (
   <HandySvg
     src={chevronRightSVG}
-    width="24"
-    height="24"
+    width="35"
+    height="35"
+    className='bonus__icon-chevron-right'
+  />
+);
+
+const IconFire = () => (
+  <HandySvg
+    src={fireSVG}
+    width="13"
+    height="17"
+    className='bonus__icon-fire'
   />
 );
 
@@ -25,17 +36,17 @@ const Bonus = () => {
 
   return (
     <div className='bonus'>
-      <section>
+      <section className='bonus__left'>
         <p className="bonus__amount">{bonusAmount} бонусов</p>
         <div className="bonus__fire">
-          <p className="bonus__date">{new Date(dateBurning).toLocaleDateString('ru-RU', options)} сгорит</p>
+          <p className="bonus__date">{new Date(dateBurning).toLocaleDateString('ru-RU', options)} сгорит </p>
+          <IconFire/>
           <p className="bonus__date">{fireBonusAmount} бонусов</p>
         </div>
       </section>
       <section className='bonus__right'>
         <IconChevronRight />
       </section>
-
     </div>
   )
 }

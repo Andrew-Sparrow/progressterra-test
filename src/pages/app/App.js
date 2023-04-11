28ed2b99f4a7fd551d6242070e81d9c4c8d0996b
Andrew-Sparrow <8911102@gmail.com>
2023-04-11 11:07:27 +0500

    adds gradient to app

---
+++ src/pages/app/App.js
@@ -0,0 +1,13 @@
+import './app.scss';
+
+function App() {
+  return (
+    <div className='app'>
+      <header className='app__header'>
+        <p className="app__logo">логотип</p>
+      </header>
+    </div>
+  );
+}
+
+export { App };

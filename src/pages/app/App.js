524ea1bd769d84575f52c862c665c6935e366708
Andrew-Sparrow <8911102@gmail.com>
2023-04-11 11:37:44 +0500

    adds icon for button info

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -1,10 +1,24 @@
+import { HandySvg } from 'handy-svg';
+
 import './app.scss';
+import iconInfoSVG from '../../icons/information-button-1.svg'
+
+const IconInfo = () => (
+  <HandySvg
+    src={iconInfoSVG}
+    width="24"
+    height="24"
+  />
+);
 
 function App() {
   return (
     <div className='app'>
       <header className='app__header'>
         <p className="app__logo">логотип</p>
+        <button className='app__button' type="button">
+          <IconInfo />
+        </button>
       </header>
     </div>
   );
